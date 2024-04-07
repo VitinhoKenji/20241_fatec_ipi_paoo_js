9cb75dae222cf13a69156d31879dd9a246a572dd
Victor Kenji <vkenjitmj@gmail.com>
2024-04-07 14:59:26 -0300

    js(funcoes): fim de 1.6.1

---
+++ index.js
@@ -0,0 +1,84 @@
+//declaração constantes
+// const nome = "José"
+// console.log(nome)
+// const endereco = 'Rua B, 21'
+// console.log(endereco)
+// const sobrenome = `Bossini`
+// console.log(sobrenome)
+// console.log(`Me chamo ${nome}. Meu sobrenome é ${sobrenome}.`)
+// const idade = 18
+// const passeiDeAno = true
+
+// let nome = "Rodrigo"
+// console.log(nome)
+// nome = "Pedro"
+// console.log(nome)
+// let a = 2
+// console.log(typeof(a))
+// a = "abc"
+// console.log(typeof(a))
+// a = true
+// console.log(typeof(a))
+
+//Não use var
+// var linguagem = "Javascript"
+// console.log(`Aprendendo ${linguagem}`)
+// var linguagem = "Java"
+// console.log(`Aprendendo ${linguagem}`)
+// var idade = 18
+// console.log(`Oi, ${nome}`)
+// if (idade >= 18){
+//     var nome = 'João' //içamento (hoist)
+//     console.log(`Paraabéns, ${nome}. Você pode dirigir!`)
+// }
+// console.log(`Até mais, ${nome}`)
+
+// //let a = "abc" javascript, dinamicamente tipado, feito pelo interpretador, em tempo de excecução
+// const n1 = 2
+// const n2 = '3'
+// //coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+// const n4 = n1 + Number(n2)
+// //coerção explícita
+// console.log(n4)
+
+//Não usar o operador de comparação ==
+// console.log(1 == '1')
+// console.log(1==='1')
+// console.log(1 == true)
+// console.log(1 == [1])
+// console.log(1 == [1,2])
+// console.log(null == null)
+// console.log(null == undefined)
+// console.log([] == false)
+// console.log([] == [])
+
+//declaração
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 3.4
+// console.log(v1.length)
+// v1[10] = 2
+// console.log(v1.length)
+// v1[2] = 'abc'
+// console.log(v1.length)
+// v1[99] = true
+// console.log(v1.length)
+// console.log(v1)
+// for (let i = 0; i<v1.length; i++){
+//     console.log(v1[i])
+// }
+//int [] v1 = new int[0];
+//var v1 = new ArrayList<list>();
+
+//funções
+//void m (){} Java
+// function hello(){
+//     console.log('oi')
+// }
+// hello()
+// function hello(nome){
+//     console.log('oi, '+nome)
+// }
+// hello('Rodrigo')
